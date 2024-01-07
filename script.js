ae092506023fc8064fc777f733eacc3f0181563c
Eyssette <cedric.eyssette@gmail.com>
2024-01-07 21:08:48 +0100

    Gestion écrans de taille moyenne

--- script.js
+++ script.js
@@ -59,12 +59,12 @@ function createBook(w, h) {
 
 function calculateDimensions() {
 	let bookWidth = (40 * window.innerWidth) / 100;
-	let bookHeight = (bookWidth / 400) * 600;
-	const maxBookHeight = (70 * window.innerHeight) / 100;
+	let bookHeight = (bookWidth / 500) * 600;
+	const maxBookHeight = (85 * window.innerHeight) / 100;
 
 	if (bookHeight > maxBookHeight) {
 		bookHeight = maxBookHeight;
-		bookWidth = (bookHeight / 600) * 400;
+		bookWidth = (bookHeight / 600) * 500;
 	}
 	return [bookWidth, bookHeight];
 }
