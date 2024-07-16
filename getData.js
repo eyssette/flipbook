ad9a46f3d957eed487d4c2aeb41e2b71b0b82c90
Eyssette <cedric.eyssette@gmail.com>
2024-07-16 02:53:03 +0200

    fix regexTitleH2

--- getData.js
+++ getData.js
@@ -303,13 +303,13 @@ function parseMarkdown(markdownContent) {
 		flipbookDataArray = markdownContentSplitted;
 		if (markdownContent.includes('!summary')) {
 			let summaryPageNumber = -1;
-			const regexTitleH2 = /## (.*)/
+			const regexTitleH2 = /(\n|^)## (.*)/
 			let titlesH2 = []
 			for (const [pageNumber,pageContent] of flipbookDataArray.entries()) {
 				if (summaryPageNumber>-1) {
 					const findTitlesH2 = pageContent.match(regexTitleH2)
 					if(findTitlesH2) {
-						const titleH2 = findTitlesH2[1]
+						const titleH2 = findTitlesH2[2]
 						titlesH2.push('<li><a href="?page='+(pageNumber+1)+'">'+titleH2+'</a></li>')
 					}
 				} else if(pageContent.includes('!summary')) {
