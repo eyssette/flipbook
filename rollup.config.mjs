b770f87f007860348f495fe4b6c80e2a6b5a84a9
Eyssette <cedric.eyssette@gmail.com>
2024-08-14 04:31:02 +0200

    nettoyage fichier config rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,39 +1,7 @@
 import terser from "@rollup/plugin-terser";
 import { string } from "rollup-plugin-string";
-//import fs from "fs";
-//import path from "path";
 import postcss from "rollup-plugin-postcss";
 import cssnano from "cssnano";
-
-// const mainMdPath = "data/main.md";
-// const mainMdContent = fs.readFileSync(mainMdPath, "utf8");
-// const otherMdFiles = getAllMdFiles("data").filter(
-// 	(file) => file !== mainMdPath
-// );
-
-// function getAllMdFiles(dir) {
-// 	const files = fs.readdirSync(dir, { withFileTypes: true });
-// 	let mdFiles = [];
-// 	for (const file of files) {
-// 		const res = path.resolve(dir, file.name);
-// 		if (file.isDirectory()) {
-// 			mdFiles = mdFiles.concat(getAllMdFiles(res));
-// 		} else if (file.name.endsWith(".md")) {
-// 			mdFiles.push(res);
-// 		}
-// 	}
-// 	return mdFiles;
-// }
-
-// function createCombinedMdFile() {
-// 	const filesContent = otherMdFiles.map((file) =>
-// 		fs.readFileSync(file, "utf8")
-// 	);
-// 	const combinedContent = [mainMdContent, ...filesContent].join("\n");
-// 	fs.writeFileSync("data.md", combinedContent);
-// }
-
-// createCombinedMdFile();
 
 export default {
 	input: "js/main.js",
