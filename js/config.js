6e764294ad46299084ce65fadc5d02b781804d47
eyssette <cedric.eyssette@gmail.com>
2024-12-16 10:23:33 +0100

    corsProxy : changement URL

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@ export const shortcuts = [
 	["modèle", "https://codimd.apps.education.fr/NuU3detpS0amHKFZ1ImX8Q"],
 ];
 
-export const corsProxy = "https://corsproxy.io/?";
+export const corsProxy = "https://corsproxy.io/?url=";
